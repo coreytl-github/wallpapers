b0433c12f3c85d2beaedbf81d2e6ea36ba070744
coreytl-github <coreytruscott@tutanota.com>
2021-11-07 21:29:02 +0000

    adding wallpapers

--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # make this script because i can never remember the command.
 # p.s i need the personal access token for this repo
+echo "Commit name?"
+read COMMIT
+git commit -m "$COMMIT"
 git push -u origin main
